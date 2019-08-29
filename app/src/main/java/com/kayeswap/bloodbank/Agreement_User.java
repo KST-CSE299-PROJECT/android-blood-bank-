64e72be52f518f6110e65ecab317a6710382cc68
Sumaiya07Shachcho <47005487+Sumaiya07Shachcho@users.noreply.github.com>
2019-08-29 23:29:43 +0600

    Update Agreement_User.java

--- app/src/main/java/com/kayeswap/bloodbank/Agreement_User.java
+++ app/src/main/java/com/kayeswap/bloodbank/Agreement_User.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.kayeswap.shahajjo.R;
+import com.kayeswap.bloodbank.R;
 
 
 public class Agreement_User extends AppCompatActivity {
